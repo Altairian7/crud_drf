39fd3c2597dbe3a50ada1ac11e7f4e43836964c8
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-13 01:45:50 +0530

    added GET request in views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#----------handling logic for API's endpoints
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import api_view
+from rest_framework import status
+from .models import User
+from .serializer import Userserializers
+
+@api_view(['GET'])
+def get_user(request):
+    return Response(Userserializers({'name': "Sean", 'age': 21}))
