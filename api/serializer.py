d17cd383bff50de2bb3a50b174e72d48e2ac61a5
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-14 15:24:11 +0530

    added CRUD operations in views.py and added 2 users by that

--- api/serializer.py
+++ api/serializer.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from .models import User
 
 
-class Userserializers(serializers.ModelSerializer):
+class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'                               #----Transforming all fields from User model
