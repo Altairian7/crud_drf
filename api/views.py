4798c8e19e69ab24d581990254260a729251d2b9
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-14 15:05:47 +0530

    Added POST request in views.py

--- api/views.py
+++ api/views.py
@@ -7,5 +7,14 @@ from .models import User
 from .serializer import Userserializers
 
 @api_view(['GET'])
-def get_user(request):
+def get_users(request):
     return Response(Userserializers({'name': "Sean", 'age': 21}).data)
+
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = Userserializers(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
