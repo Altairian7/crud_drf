1ee532fdf41b1c15fe5751cca6b82ec560908172
altairian7 <artisharma.as12.as@gmail.com>
2025-05-01 23:44:23 +0530

    added update and delete mutation

--- graphql_flask/mutations.py
+++ graphql_flask/mutations.py
@@ -14,3 +14,39 @@ class CreateUser(graphene.Mutation):
         new_user = User(id=len(users_db) + 1, name=name, email=email)
         users_db.append(new_user)
         return CreateUser(user=new_user)
+
+
+# mutations.py (add this)
+class UpdateUser(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        name = graphene.String()
+        email = graphene.String()
+
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, id, name=None, email=None):
+        user = next((u for u in users_db if u.id == id), None)
+        if user:
+            if name:
+                user.name = name
+            if email:
+                user.email = email
+        return UpdateUser(user=user)
+
+class DeleteUser(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, id):
+        global users_db
+        users_db = [u for u in users_db if u.id != id]
+        return DeleteUser(ok=True)
+
+# Register them
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+    update_user = UpdateUser.Field()
+    delete_user = DeleteUser.Field()
