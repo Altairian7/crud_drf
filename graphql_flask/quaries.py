0318aa25cfd0a513ca7850f33ecc9155b04bb03f
altairian7 <artisharma.as12.as@gmail.com>
2025-05-01 23:42:46 +0530

    add query to fetch user by ID

--- graphql_flask/quaries.py
+++ graphql_flask/quaries.py
@@ -11,3 +11,10 @@ class Query(graphene.ObjectType):
 
     def resolve_users(self, info):
         return users_db
+
+
+# queries.py (add to Query class)
+user_by_id = graphene.Field(UserType, id=graphene.Int())
+
+def resolve_user_by_id(self, info, id):
+    return next((user for user in users_db if user.id == id), None)
