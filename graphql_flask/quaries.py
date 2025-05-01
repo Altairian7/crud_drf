b278eb10c122210fefa7cd820c020ae07afae271
altairian7 <artisharma.as12.as@gmail.com>
2025-05-01 23:42:17 +0530

    updated quaries to fetch all users

--- graphql_flask/quaries.py
+++ graphql_flask/quaries.py
@@ -1,8 +1,13 @@
-# queries.py
-import graphene
+# queries.py (update this)
+from types import UserType
+from models import users_db
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(description="A simple hello world")
+    hello = graphene.String()
+    users = graphene.List(UserType)
 
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
+
+    def resolve_users(self, info):
+        return users_db
