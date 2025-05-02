a9ffb3b471f2fe1474926f3f2c3e1c748f0fbe9a
altairian7 <artisharma.as12.as@gmail.com>
2025-05-02 23:54:44 +0530

    changed the entire course of app.py just to fix an issue

--- taskmanager_api/app.py
+++ taskmanager_api/app.py
@@ -1,73 +1,35 @@
 from flask import Flask, request, jsonify
-from ariadne import graphql_sync, make_executable_schema, ObjectType, QueryType, MutationType, load_schema_from_path
-from ariadne.constants import PLAYGROUND_HTML
-from models import db, Task
+from ariadne import QueryType, MutationType, make_executable_schema, graphql_sync, load_schema_from_path
+from ariadne.explorer import ExplorerGraphiQL
 
-# Flask App
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-
-# Schema
-type_defs = load_schema_from_path("schema.graphql")
+from models import tasks, create_task
 
 query = QueryType()
 mutation = MutationType()
-task_obj = ObjectType("Task")
 
-# Query: Get all tasks
 @query.field("tasks")
 def resolve_tasks(_, info):
-    return Task.query.all()
+    return tasks
 
-# Query: Get single task
-@query.field("task")
-def resolve_task(_, info, id):
-    return Task.query.get(id)
-
-# Mutation: Create
 @mutation.field("createTask")
 def resolve_create_task(_, info, title, dueDate=None):
-    task = Task(title=title, due_date=dueDate)
-    db.session.add(task)
-    db.session.commit()
-    return task
+    return create_task(title, dueDate)
 
-# Mutation: Toggle Complete
-@mutation.field("toggleDone")
-def resolve_toggle_done(_, info, id):
-    task = Task.query.get(id)
-    task.is_done = not task.is_done
-    db.session.commit()
-    return task
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(type_defs, query, mutation)
 
-# Mutation: Delete
-@mutation.field("deleteTask")
-def resolve_delete(_, info, id):
-    task = Task.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return True
+app = Flask(__name__)
+explorer_html = ExplorerGraphiQL().html(None)
 
-
-# Schema
-schema = make_executable_schema(type_defs, [query, mutation, task_obj])
-
-# Routes
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
-    return PLAYGROUND_HTML, 200
+    return explorer_html, 200
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(schema, data, context_value=request)
-    return jsonify(result)
-
-# Create DB
-with app.app_context():
-    db.create_all()
+    success, result = graphql_sync(schema, data, context_value=request, debug=True)
+    return jsonify(result), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
