b1cb2fa5f4f7fe028f999e308bbe0ebb95fb6da8
altairian7 <artisharma.as12.as@gmail.com>
2025-05-02 23:47:18 +0530

    define schema and create 2 queries

--- taskmanager_api/app.py
+++ taskmanager_api/app.py
@@ -8,3 +8,20 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
+# Schema
+type_defs = load_schema_from_path("schema.graphql")
+
+query = QueryType()
+mutation = MutationType()
+task_obj = ObjectType("Task")
+
+# Query: Get all tasks
+@query.field("tasks")
+def resolve_tasks(_, info):
+    return Task.query.all()
+
+# Query: Get single task
+@query.field("task")
+def resolve_task(_, info, id):
+    return Task.query.get(id)
